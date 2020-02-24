1b7b74c2d9ff5d90d6afb42d457a9889ee4cf235
TheGoddessInari <thegoddessinari@gmail.com>
2020-02-24 06:58:13 -0800

    Unify defaultSites, update to work on both Firefox and Chrome.

--- background.js
+++ background.js
@@ -1,105 +1,5 @@
 'use strict';
 
-var defaultSites = {
-  'Algemeen Dagblad': 'ad.nl',
-  'Baltimore Sun': 'baltimoresun.com',
-  'Barron\'s': 'barrons.com',
-  'Bloomberg': 'bloomberg.com',
-  'Bloomberg Quint': 'bloombergquint.com',
-  'Business Insider': 'businessinsider.com',
-  'Caixin': 'caixinglobal.com',
-  'Chemical & Engineering News': 'cen.acs.org',
-  'Chicago Tribune': 'chicagotribune.com',
-  'Central Western Daily': 'centralwesterndaily.com.au',
-  'Crain\'s Chicago Business': 'chicagobusiness.com',
-  'Corriere Della Sera': 'corriere.it',
-  'Daily Press': 'dailypress.com',
-  'DeMorgen': 'demorgen.be',
-  'Denver Post': 'denverpost.com',
-  'De Tijd': 'tijd.be',
-  'De Groene Amsterdammer': 'groene.nl',
-  'de Volkskrant': 'volkskrant.nl',
-  'ET Prime': 'prime.economictimes.indiatimes.com',
-  'The Economist': 'economist.com',
-  'Eindhovens Dagblad': 'ed.nl',
-  'Examiner': 'examiner.com.au',
-  'Financial Times': 'ft.com',
-  'Foreign Policy': 'foreignpolicy.com',
-  'Glassdoor': 'glassdoor.com',
-  'Haaretz': 'haaretz.co.il',
-  'Haaretz English': 'haaretz.com',
-  'Handelsblatt': 'handelsblatt.com',
-  'Harper\'s Magazine': 'harpers.org',
-  'Hartford Courant': 'courant.com',
-  'Harvard Business Review': 'hbr.org',
-  'Inc.com': 'inc.com',
-  'Investors Chronicle': 'investorschronicle.co.uk',
-  'La Repubblica': 'repubblica.it',
-  'La Stampa': 'lastampa.it',
-  'Le Monde': 'lemonde.fr',
-  'Le Parisien': 'leparisien.fr',
-  'London Review of Books': 'lrb.co.uk',
-  'Los Angeles Times': 'latimes.com',
-  'Medium': 'medium.com',
-  'Medscape': 'medscape.com',
-  'MIT Technology Review': 'technologyreview.com',
-  'Mountain View Voice': 'mv-voice.com',
-  'National Post': 'nationalpost.com',
-  'New Statesman': 'newstatesman.com',
-  'New York Magazine': 'nymag.com',
-  'Nikkei Asian Review': 'asia.nikkei.com',
-  'NK News': 'nknews.org',
-  'NRC': 'nrc.nl',
-  'New Zealand Herald': 'nzherald.co.nz',
-  'Orange County Register': 'ocregister.com',
-  'Orlando Sentinel': 'orlandosentinel.com',
-  'Palo Alto Online': 'paloaltoonline.com',
-  'Parool': 'parool.nl',
-  'Quartz': 'qz.com',
-  'Quora': 'quora.com',
-  'Scientific American': 'scientificamerican.com',
-  'Statista': 'statista.com',
-  'SunSentinel': 'sun-sentinel.com',
-  'Tech in Asia': 'techinasia.com',
-  'Telegraaf': 'telegraaf.nl',
-  'The Advocate': 'theadvocate.com.au',
-  'The Age': 'theage.com.au',
-  'The Atlantic': 'theatlantic.com',
-  'The Australian': 'theaustralian.com.au',
-  'The Australian Financial Review': 'afr.com',
-  'The Boston Globe': 'bostonglobe.com',
-  'The Business Journals': 'bizjournals.com',
-  'The Daily Telegraph': 'dailytelegraph.com.au',
-  'The Diplomat': 'thediplomat.com',
-  'The Globe and Mail': 'theglobeandmail.com',
-  'The Herald': 'theherald.com.au',
-  'The Hindu': 'thehindu.com',
-  'The Japan Times': 'japantimes.co.jp',
-  'TheMarker': 'themarker.com',
-  'The Mercury Tasmania': 'themercury.com.au',
-  'The Mercury News': 'mercurynews.com',
-  'The Morning Call': 'mcall.com',
-  'The Nation': 'thenation.com',
-  'The New York Times': 'nytimes.com',
-  'The New Yorker': 'newyorker.com',
-  'The News-Gazette': 'news-gazette.com',
-  'The Philadelphia Inquirer': 'inquirer.com',
-  'The Saturday Paper': 'thesaturdaypaper.com.au',
-  'The Spectator': 'spectator.co.uk',
-  'The Seattle Times': 'seattletimes.com',
-  'The Sydney Morning Herald': 'smh.com.au',
-  'The Telegraph': 'telegraph.co.uk',  
-  'The Times': 'thetimes.co.uk',
-  'The Toronto Star': 'thestar.com',
-  'The Washington Post': 'washingtonpost.com',
-  'The Wall Street Journal': 'wsj.com',
-  'Times Literary Supplement': 'the-tls.co.uk',
-  'Towards Data Science': 'towardsdatascience.com',
-  'Trouw': 'trouw.nl',
-  'Vanity Fair': 'vanityfair.com',
-  'Vrij Nederland': 'vn.nl',
-  'Wired': 'wired.com',
-};
 
 const restrictions = {
   'barrons.com': /.+barrons\.com\/articles\/.+/,
@@ -224,10 +124,10 @@ const use_google_bot = [
 ]
 
 function setDefaultOptions() {
-  browser.storage.sync.set({
+  chrome.storage.local.set({
     sites: defaultSites
   }, function() {
-    browser.runtime.openOptionsPage();
+    chrome.runtime.openOptionsPage();
   });
 }
 
@@ -256,7 +156,7 @@ const userAgentMobile = "Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible ; 
 var enabledSites = [];
 
 // Get the enabled sites
-browser.storage.sync.get({
+chrome.storage.local.get({
   sites: {}
 }, function(items) {
   var sites = items.sites;
@@ -266,7 +166,7 @@ browser.storage.sync.get({
 });
 
 // Listen for changes to options
-browser.storage.onChanged.addListener(function(changes, namespace) {
+chrome.storage.onChanged.addListener(function(changes, namespace) {
   var key;
   for (key in changes) {
     var storageChange = changes[key];
@@ -280,7 +180,7 @@ browser.storage.onChanged.addListener(function(changes, namespace) {
 });
 
 // Set and show default options on install
-browser.runtime.onInstalled.addListener(function(details) {
+chrome.runtime.onInstalled.addListener(function(details) {
   if (details.reason == "install") {
     setDefaultOptions();
   } else if (details.reason == "update") {
@@ -289,7 +189,7 @@ browser.runtime.onInstalled.addListener(function(details) {
 });
 
 // Disable javascript for these sites
-browser.webRequest.onBeforeRequest.addListener(function(details) {
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
   if (!isSiteEnabled(details) || details.url.indexOf("mod=rsswn") !== -1) {
     return;
   }
@@ -302,7 +202,7 @@ browser.webRequest.onBeforeRequest.addListener(function(details) {
   ["blocking"]
 );
 
-browser.webRequest.onBeforeSendHeaders.addListener(function(details) {
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
   var requestHeaders = details.requestHeaders;
 
   var header_referer = '';
@@ -403,11 +303,11 @@ browser.webRequest.onBeforeSendHeaders.addListener(function(details) {
 
   if (tabId !== -1) {
     // run contentScript inside tab
-    browser.tabs.executeScript(tabId, {
+    chrome.tabs.executeScript(tabId, {
       file: 'contentScript.js',
       runAt: 'document_start'
     }, function(res) {
-      if (browser.runtime.lastError || res[0]) {
+      if (chrome.runtime.lastError || res[0]) {
         return;
       }
     });
@@ -419,16 +319,17 @@ browser.webRequest.onBeforeSendHeaders.addListener(function(details) {
 }, ['blocking', 'requestHeaders']);
 
 // remove cookies after page load
-browser.webRequest.onCompleted.addListener(function(details) {
-  for (var domainIndex in remove_cookies) {
-    var domainVar = remove_cookies[domainIndex];
+chrome.webRequest.onCompleted.addListener(function(details) {
+  for (var domainVar of remove_cookies) {
     if (!enabledSites.includes(domainVar) || details.url.indexOf(domainVar) === -1) {
       continue; // don't remove cookies
     }
-    browser.cookies.getAll({domain: domainVar}, function(cookies) {
+    chrome.cookies.getAll({domain: domainVar}, function(cookies) {
       for (var i=0; i<cookies.length; i++) {
+        var cookie_domain = cookies[i].domain;
+        var stripped_domain = cookie_domain.replace(/^(\.)/, '');
         var cookie = {
-          url: (cookies[i].secure ? "https://" : "http://") + cookies[i].domain + cookies[i].path,
+          url: (cookies[i].secure ? "https://" : "http://") + stripped_domain + cookies[i].path,
           name: cookies[i].name,
           storeId: cookies[i].storeId
         };
@@ -436,7 +337,6 @@ browser.webRequest.onCompleted.addListener(function(details) {
           cookie.firstPartyDomain = cookies[i].firstPartyDomain;
         }
 
-		var cookie_domain = cookies[i].domain;
 		var rc_domain = cookie_domain.replace(/^(\.?www\.|\.)/, '');
 		// hold specific cookie(s) from remove_cookies domains
 		if ((rc_domain in remove_cookies_select_hold) && remove_cookies_select_hold[rc_domain].includes(cookies[i].name)){
@@ -445,8 +345,8 @@ browser.webRequest.onCompleted.addListener(function(details) {
 		// drop only specific cookie(s) from remove_cookies domains
 		if ((rc_domain in remove_cookies_select_drop) && !(remove_cookies_select_drop[rc_domain].includes(cookies[i].name))){
 			continue; // only remove specific cookie
-		}
-        browser.cookies.remove(cookie);
+    }
+        chrome.cookies.remove(cookie);
       }
     });
   }
